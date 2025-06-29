54857f0a0b100eb4d9027e751eea45601793020c
goldshoot0720 <goldshoot0720@gmail.com>
2025-06-29 20:05:16 +0800

    tsaopaofenghsiung2025

---
+++ tsaopaofenghsiung2025-git.bat
@@ -0,0 +1,8 @@
+cd "F:\tsaopaofenghsiung2025-master"
+git init
+git add .
+git commit -m "tsaopaofenghsiung2025"
+git remote add origin https://github.com/abuhg17/tsaopaofenghsiung2025.git
+git checkout -b master
+git push -u origin master --force
+pause
